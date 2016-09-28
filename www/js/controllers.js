e2a291cccaa1ad9f9dcd8d5211ba0aa0553aa5b8
Ransom Christofferson <ransoing@gmail.com>
2016-09-27 22:01:40 -0600

    Fixed android styling and external links now work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope) {})
+.controller('MainCtrl', function($scope) {
+  $scope.openExternalLink = openExternalLink;
+})
 
-.controller('ReportCtrl', function($scope) {})
+.controller('ReportCtrl', function($scope) {
+  $scope.openExternalLink = openExternalLink;
+})
 
 .controller('RedFlagsCtrl', function($scope) {})
 
 .controller('AboutTraffickingCtrl', function($scope) {})
+
+// for some reason I couldn't get the app to trigger mailto: and tel: links properly
+// using the href attribute, so instead I open the link using window.open
+// I also had issues opening external URLs in the default browser
+var openExternalLink = function( url ) {
+  window.open( url, '_system' );
+};
